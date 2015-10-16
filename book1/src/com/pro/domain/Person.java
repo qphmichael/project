2cbc0c692ed97c07e80fb0eab4899b1cf293bd1b
戚平航 <dell@192.168.1.68>
2015-10-16 14:46:09 +0800

    创建了一个book1项目

---
+++ book1/src/com/pro/domain/Person.java
@@ -0,0 +1,18 @@
+package com.pro.domain;
+
+public class Person {
+		private int personId;
+		private String personName;
+		public int getPersonId() {
+			return personId;
+		}
+		public void setPersonId(int personId) {
+			this.personId = personId;
+		}
+		public String getPersonName() {
+			return personName;
+		}
+		public void setPersonName(String personName) {
+			this.personName = personName;
+		}
+}
